56ce9e6e3afcb64458be013fe95940812180c3dc
gpvsystems <gpvsystems@gmail.com>
2026-01-16 13:14:03 -0300

    ajuste de errores y excepciones

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const ProductosService = require('./Aplicacion/Servicios/productosServices.js');
-const productosService = new ProductosService();  
+const productosService = new ProductosService();
 
 
 // Middleware para parsear JSON
@@ -15,17 +15,29 @@ app.get('/', (req, res) => {
 
 app.get('/api/productos', async (req, res) => {
 
-  let productos = await productosService.obtenerTodosLosProductos();
- /*  productos = []; // Simular sin productos */
 
- console.log(productos);
-  if(productos.length === 0) {
-    return res.status(404).json({ mensaje: 'No se encontraron productos', productos: [] });
-  }else{
-    return res.status(200).json({ mensaje: 'Lista de productos', productos: productos });
+  try {
+    let productos = await productosService.obtenerTodosLosProductos();
+    /*  productos = []; // Simular sin productos */
+
+    
+    if (productos.length === 0) {
+      // si no hay productos
+      return res.status(404).json({ mensaje: 'No se encontraron productos', productos: [] });
+    } else {
+      // si hay productos
+      return res.status(200).json({ mensaje: 'Lista de productos', productos: productos });
+    }
+
+    res.json({ mensaje: 'Lista de productos', productos: productos });
+
+  } catch (error) {
+    //si hay un error en el servidor
+    console.error('Error al obtener los productos:', error);
+    res.status(500).json({ mensaje: 'Error interno del servidor' });
+
   }
 
-  res.json({ mensaje: 'Lista de productos', productos: productos });
 });
 
 
